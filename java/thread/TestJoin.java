29259eca3062612c5a7e3cd106a5c6b8d776368f
wangxuan <wangxuan19911119@163.com>
2017-11-22 10:32:26 +0800

    保证三个线程依次按顺序执行

---
+++ java/thread/TestJoin.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.*;
+
+public class TestJoin {
+    public static void main(String[] args) throws InterruptedException {
+        final Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " run 1");
+            }
+        }, "T1");
+        final Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " run 2");
+                try {
+                    t1.join(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "T2");
+        final Thread t3 = new Thread(new Runnable() {
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " run 3");
+                try {
+                    t2.join(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "T3");
+        // method1
+        //t1.start();
+        //t2.start();
+        //t3.start();
+
+        // method 2 使用单个任务的线程池来实现，保证线程的依次执行。
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(t1);
+        executor.submit(t2);
+        executor.submit(t3);
+        executor.shutdown();
+    }
+}
